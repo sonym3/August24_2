b52c45688b812dc207552dda359d4cdf0ca871df
1895268 <1895268@E21047.CEGEPGIM.LCL>
2019-08-25 10:56:04 -0400

    Main Class done for client class

--- src/august24_2/August24_2.java
+++ src/august24_2/August24_2.java
@@ -33,25 +33,26 @@ public class August24_2 {
           Statement stmt = null;
           DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
           con=DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "sales", "anypw");
-           
+          Client c = new Client();
            
            
            /*
            //DELIVERY CLASS
            String str = "2013-09-04 13:30:00";
-           int noitem = 28;
+           int noitem = 27;
            d.insertDelivery(con, stm, noitem, str);
-           */
-           int nodelivery = 101;
-           d.deleteDelivery(con, stm, nodelivery);
-           
-           /*
-           int no_delivery =104; 
-           String datedelivery = "2013-09-04 13:30:00";
+         
+          int no_delivery =27; 
+           String datedelivery = "2001-01-01 01:01:01";
            d.updateDelivery(con, stm, no_delivery, datedelivery);
            
            
+           int nodelivery = 27;
+           d.deleteDelivery(con, stm, nodelivery);
+         
+           */
            
+          /* 
            //DETAILEDDELIVERY CLASS
            int no_delivery2 = 104;
            dd.deleteDetaileDelivery(con, stm, no_delivery2);
@@ -61,6 +62,30 @@ public class August24_2 {
            dd.updateDetaileDelivery(con, stm, no_delivery3, no_item2);
      
            */
+          
+          //CLIENT CLASS
+          
+            Double noclient2=97.0;
+            String nameclient2="Asma";
+            String notelephone2= "12345678";
+            
+            c.clientInsert(con, stm, noclient2, nameclient2, notelephone2);
+             
+            String nameclient =  "ASMAROKADIYA";
+            String notelephone ="789654123";
+            Double noclient=97.0;
+            
+            c.clientUpdate(con, stm, nameclient, notelephone, noclient);
+            
+       
+            int noclient3 = 97;
+            c.clientDelete(con, stm, noclient3);
+             
+             c.clientList(con, stmt);   
+             
+             int noclient4=60;
+             c.clientSingle(con, stm, noclient4);
+       
            
     } catch (SQLException ex) {
             Logger.getLogger(August24_2.class.getName()).log(Level.SEVERE, null, ex);
